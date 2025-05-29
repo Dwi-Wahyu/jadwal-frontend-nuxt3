b88904fbc2ecacdfc32eae29ced94369c054afaf
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-05-29 12:54:34 +0800

    fix nama aplikasi

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "E-Jadwal FKG", // Nama aplikasi
+      name: "E-Jadwal Frontend", // Nama aplikasi
       script: ".output/server/index.mjs", // Path ke file yang dijalankan
       env: {
         NITRO_PORT: 3000,
